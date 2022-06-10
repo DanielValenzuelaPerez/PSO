d5016be42ba6801ec6441c15da73970d57545dcc
MabYng <DanielValenzuelaPerez@outlook.com>
2022-06-10 14:05:55 -0500

    Working version 1 ^_^

--- particle.py
+++ particle.py
@@ -1,7 +1,60 @@
+from random import random
+
 class Particle:
-    def __init__(self, x):
-        self.x = x
-        print("Hello! My position is at: " + str(self.x))
+    global_best_fitness = None
+    global_best_position = None
+    def __init__(self):
+        self.x = [random() * 5, random() * 5]
+        self.v = [random(), random()]
+        self.x = [self.x[0] + self.v[0], self.x[1] + self.v[1]] # Update position (pos + vel)
+
+        self.w = 0.8
+        self.r = [random(), random()]
+        self.c = [0.1, 0.1]
+
+        self.personal_best_fitness = None
+        self.personal_best_position = self.x.copy()
+
+        self.current_fitness = None
     
-    def update(self):
-        print("Swarm formation BEGIN! " + str(self.x))
+    def move(self):
+        if Particle.global_best_fitness == None:
+            return
+
+        for i in range(len(self.v)):
+            self.v[i] = self.w*self.v[i] + self.c[0]*self.r[0]*(self.personal_best_position[i] - self.x[i]) + self.c[1]*self.r[1]*(Particle.global_best_position[i] - self.x[i])
+            self.x[i] = self.x[i] + self.v[i]
+    
+    def update_fitness(self, fitness):
+        self.current_fitness = fitness
+
+        if self.personal_best_fitness == None:
+            self.personal_best_fitness = fitness
+        if Particle.global_best_fitness == None:
+            Particle.global_best_fitness = self.personal_best_fitness 
+            Particle.global_best_position = self.personal_best_position.copy()
+        
+        if fitness < self.personal_best_fitness:
+            self.personal_best_fitness = fitness
+            self.personal_best_position = self.x.copy()
+
+        if self.personal_best_fitness < Particle.global_best_fitness:
+            Particle.global_best_fitness = self.personal_best_fitness
+            Particle.global_best_position = self.personal_best_position.copy()
+        
+        self.printProgress()
+    
+    def printArray(self, array):
+        msg = ""
+        msg += "[{:.2f}".format(array[0]) + ", {:.2f}".format(array[1]) + "]\t"
+        return msg
+    
+    def printProgress(self):
+        msg = "(" + "{:.2f}".format(self.current_fitness) + ")"
+        msg += self.printArray(self.x)
+        msg += self.printArray(self.v)
+        msg += "(" + "{:.2f}".format(self.personal_best_fitness) + ")"
+        msg += self.printArray(self.personal_best_position)
+        msg += "(" + "{:.2f}".format(Particle.global_best_fitness) + ")"
+        msg += self.printArray(Particle.global_best_position)
+        print(msg)
--- swarm.py
+++ swarm.py
@@ -1,14 +1,20 @@
 from particle import Particle
 class Swarm:
     def __init__(self, number_of_particles):
-        self.number_of_paritcles = number_of_particles
-        print("Creating constructore with " + str(self.number_of_paritcles) + " particles.")
+        self.number_of_particles = number_of_particles
+        print("Creating constructore with " + str(self.number_of_particles) + " particles.")
         self.particle = []
-        for i in range(self.number_of_paritcles):
-            p = Particle(i+1)
+        for i in range(self.number_of_particles):
+            p = Particle()
             self.particle.append(p)
     
-    def run_swarm_on_plane(self, plane):
+    def run_swarm_on_plane(self, plane, iterations):
+        iteration = 0
         if(plane == 1):
-            for i in range(self.number_of_paritcles):
-                self.particle[i].update()
+            while iteration < iterations:
+                print("--CURRENT FIT & POS-------VELOCITY---------PERSONAL BEST-----------GLOBAL BEST----")
+                for i in range(self.number_of_particles):
+                    self.particle[i].move()
+                    fitness = pow(self.particle[i].x[0], 2) + pow(self.particle[i].x[1], 2) # function
+                    self.particle[i].update_fitness(fitness)
+                iteration += 1
